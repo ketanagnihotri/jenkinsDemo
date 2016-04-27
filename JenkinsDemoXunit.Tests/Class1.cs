62b82a471d6c5866869f657c5fc810b401fdf4e3
Ketan Agnihotri <kagnihotri@internal.tavisca.com>
2016-04-27 14:58:35 +0530

    Xunit added

--- JenkinsDemoXunit.Tests/Class1.cs
+++ JenkinsDemoXunit.Tests/Class1.cs
@@ -18,7 +18,7 @@ namespace JenkinsDemoXunit.Tests
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(5, Add(2, 2));
         }
 
         int Add(int x, int y)
